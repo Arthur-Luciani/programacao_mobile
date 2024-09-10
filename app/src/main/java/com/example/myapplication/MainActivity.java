3217be4a7e6ddba5bfb8bce305fe0867314b6bf7
arthur <arthurflcorrea@gmail.com>
2024-09-09 21:02:47 -0300

    Contador cliques

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int cont = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button butao = findViewById(R.id.butao);
+        butao.setText("Click here");
+
+        TextView texto = findViewById(R.id.texto);
+        texto.setText("Total cliques " + cont);
+
+        butao.setOnClickListener(view -> texto.setText("Total cliques " + cont++));
+
+    }
+
+
+}
