131160bb48f1673be7bef99c0c953ca994620c4f
arthur <arthurflcorrea@gmail.com>
2025-01-22 23:32:00 -0300

    Listagem

---
+++ listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.listagem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnList;
+    Button btnRecycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnList = findViewById(R.id.btnList);
+        btnRecycler = findViewById(R.id.btnRec);
+
+        setupButton(btnList, List.class);
+        setupButton(btnRecycler, Recycler.class);
+
+    }
+
+    private void setupButton(Button btn, Class<?> clazz) {
+        btn.setOnClickListener(v -> startActivity(new Intent(this.getApplicationContext(), clazz)));
+    }
+}
